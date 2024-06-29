90015898e697e4eb41278c9613241da795e7ae09
JunyaoC <junyaoxiandingchan@gmail.com>
2024-06-29 17:18:36 +0800

    campaign ui

--- src/components/campaign/CampaignLink.vue
+++ src/components/campaign/CampaignLink.vue
@@ -1,8 +1,15 @@
 <template>
 
-    <button class="border px-4 py-2 rounded-xl hover:bg-blue-500 hover:text-white hover:ring-2 hover:ring-blue-300 hover:ring-offset-2 duration-300 hover:scale-95 active:scale-75 flex justify-center items-center space-x-2 ">
-        <iconify-icon icon="octicon:link-16"></iconify-icon>
-        <span class="text-sm">{{ link_text }}</span>
+    <button @click="copy()" class="border px-4 pl-2 py-2 rounded-xl hover:bg-blue-500 hover:text-white hover:ring-2 hover:ring-blue-300 duration-300 active:scale-90 flex justify-center items-center space-x-2 bg-white ">
+
+        <div :class="[copied?'opacity-100':'opacity-0', 'absolute flex justify-center items-center']" >
+            <iconify-icon class="text-2xl" icon="octicon:check-16"></iconify-icon>
+            <span class="ml-2 text-xl font-brand">Copied</span>
+        </div>
+        <div :class="[copied?'opacity-0':'opacity-100','flex justify-center items-center']">
+            <iconify-icon class="text-2xl" icon="octicon:link-16"></iconify-icon>
+            <span class="ml-3 text-lg font-brand">{{ link_text }}</span>
+        </div>
 
     </button>
 
@@ -14,5 +21,8 @@
 
     const props = defineProps(['link_text'])
     const { link_text } = toRefs(props)
+
+    import { useClipboard } from '@vueuse/core'
+    const { text, copy, copied, isSupported } = useClipboard({ source: link_text.value })
 
 </script>
--- src/components/campaign/CampaignTable.vue
+++ src/components/campaign/CampaignTable.vue
@@ -1,27 +1,55 @@
 <template>
   <div v-if="steps?.length > 0">
     <nav aria-label="Progress">
-      <ol role="list" class="divide-y divide-gray-300 border-y border-gray-300 md:flex md:divide-y-0">
-        <li @click="filterStep(stepIdx)" v-for="(step, stepIdx) in steps" :key="step.name" class="relative md:flex md:flex-1 duration-300 cursor-pointer" :class="[isSelectedFilter(stepIdx) ?'bg-blue-50 hover:bg-blue-100':'hover:bg-gray-100']">
+      <ol role="list" class="divide-y  bg-stone-800 text-stone-50  md:flex md:divide-y-0">
+        <li @click="filterStep(stepIdx)" v-for="(step, stepIdx) in steps" :key="step.name" class="relative md:flex md:flex-1 cursor-pointer group pl-6 -ml-6 " :class="[isSelectedFilter(stepIdx) ?'bg-blue-500':'hover:bg-stone-700 hover:border-l  hover:border-l-stone-800']">
           <div class="group flex w-full items-center">
             <div class="flex items-center justify-between px-6 py-4 text-sm font-medium">
-              <p class="font-brand text-xl">{{ step.count }}</p>
+              
+              <div class="flex justify-start items-center space-x-2">
+                <span class="font-brand text-xl">{{ step.count }}</span>
+                <iconify-icon icon="mdi:user" class="text-stone-400 text-3xl" />
+              </div>
 
               <div class="ml-4">
-                <div class="text-sm font-medium text-gray-900">{{ step.name }}</div>
-                <div class="text-sm text-yellow-600" v-if="getSubtitle(stepIdx)">
+                <div class="text-lg font-medium text-white">{{ step.name }}</div>
+                <div class="text-sm text-stone-400" v-if="getSubtitle(stepIdx)">
                   {{ getSubtitle(stepIdx) }}
                 </div>
               </div>
             </div>
           </div>
-          <template v-if="stepIdx !== steps.length - 1">
+          <template v-if="true">
             <!-- Arrow separator for lg screens and up -->
-            <div class="absolute right-0 top-0 hidden h-full w-5 md:block" aria-hidden="true">
-              <svg class="h-full w-full text-gray-300 bg-white" viewBox="0 0 22 80" fill="none" preserveAspectRatio="none">
-                <path d="M0 -2L20 40L0 82" vector-effect="non-scaling-stroke" stroke="currentcolor" stroke-linejoin="round" />
-              </svg>
-            </div>
+
+            <template v-if="isSelectedFilter(stepIdx)">
+
+              <div class="absolute right-0 top-0 hidden h-full w-5 md:block duration-0" aria-hidden="true">
+                <svg class="h-full w-full text-stone-500 bg-stone-800" viewBox="0 0 22 80" fill="#3b82f6" preserveAspectRatio="none">
+                  <path d="M0 -2L20 40L0 82" vector-effect="non-scaling-stroke" stroke="currentcolor" stroke-linejoin="round" />
+                </svg>
+              </div>
+
+            </template>
+
+            <template v-else>
+
+              <div class="group-hover:opacity-0 absolute right-0 top-0 hidden h-full w-5 md:block duration-0" aria-hidden="true">
+                <svg class="h-full w-full text-stone-500 bg-stone-800" viewBox="0 0 22 80" fill="#292524" preserveAspectRatio="none">
+                  <path d="M0 -2L20 40L0 82" vector-effect="non-scaling-stroke" stroke="currentcolor" stroke-linejoin="round" />
+                </svg>
+              </div>
+  
+              <div class="group-hover:opacity-100 opacity-0 absolute right-0 top-0 hidden h-full w-5 md:block duration-0" aria-hidden="true">
+                <svg class="h-full w-full text-stone-500 bg-stone-800" viewBox="0 0 22 80" fill="#44403c" preserveAspectRatio="none">
+                  <path d="M0 -2L20 40L0 82" vector-effect="non-scaling-stroke" stroke="currentcolor" stroke-linejoin="round" />
+                </svg>
+              </div>
+            </template>
+
+
+
+            
           </template>
         </li>
 
@@ -37,17 +65,17 @@
     </nav>
 
     <div class="w-full">
-    <div class="border border-t-0">
+    <div class="">
       <Table>
-        <TableHeader>
-          <TableRow v-for="headerGroup in table.getHeaderGroups()" :key="headerGroup.id">
+        <TableHeader class="bg-stone-700">
+          <TableRow class="hover:bg-stone-700" v-for="headerGroup in table.getHeaderGroups()" :key="headerGroup.id">
             <TableHead v-for="header in headerGroup.headers" :key="header.id">
               <FlexRender v-if="!header.isPlaceholder" :render="header.column.columnDef.header" :props="header.getContext()" />
             </TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
-          <TableRow v-for="row in table.getRowModel().rows" :key="row.id">
+          <TableRow class="hover:bg-stone-800" v-for="row in table.getRowModel().rows" :key="row.id">
             <TableCell v-for="cell in row.getVisibleCells()" :key="cell.id">
               <FlexRender :render="cell.column.columnDef.cell" :props="cell.getContext()" />
             </TableCell>
@@ -108,40 +136,83 @@ const customGreaterThanOrEqual = (rowValue, columnValue, filterValue) => {
 };
 
 const columns = [
-  
   {
     accessorKey: 'address',
-    header: () => 'Address',
-    cell: ({ row }) => h('div', {}, `${String(row.getValue('address')).substring(0,10)}...`),
+    header: () => h('div', {
+      class: 'text-blue-200 font-brand'
+    }, 'Address'),
+    cell: ({ row }) => h('div', {
+      class: 'text-white'
+    }, `${String(row.getValue('address')).substring(0,10)}...`),
   },
   {
     accessorKey: 'id',
-    header: () => 'ID',
-    cell: ({ row }) => h('div', {}, row.getValue('id')),
+    header: () => h('div', {
+      class: 'text-blue-200 font-brand'
+    }, 'ID'),
+    cell: ({ row }) => h('div', {
+      class: 'text-white'
+    }, row.getValue('id')),
   },
   {
     accessorKey: 'name',
-    header: () => 'Name',
-    cell: ({ row }) => h('div', {}, row.getValue('name')),
+    header: () => h('div', {
+      class: 'text-blue-200 font-brand'
+    }, 'Name'),
+    cell: ({ row }) => h('div', {
+      class: 'text-white'
+    }, row.getValue('name')),
   },
   {
     accessorKey: 'progress',
-    // header: () => 'Progress',
     filterFn: customGreaterThanOrEqual,
     header: ({ column }) => {
-            return h(Button, {
-                variant: 'ghost',
-                onClick: () => column.toggleSorting(column.getIsSorted() === 'asc'),
-            }, () => ['Progress', h(ArrowUpDown, { class: 'ml-2 h-4 w-4' })])
-        },
+      return h(Button, {
+        variant: 'ghost',
+        class: 'hover:bg-stone-800 text-white hover:text-white',
+        onClick: () => column.toggleSorting(column.getIsSorted() === 'asc'),
+      }, () => [
+        h('div', {
+          class: 'text-blue-200 font-brand'
+        }, 'Progress'),
+        h(ArrowUpDown, { class: 'ml-2 h-4 w-4' })
+      ])
+    },
     cell: ({ row }) => {
       const progress = row.getValue('progress');
       return h('div', {
-        class:'pl-4'
-      }, `${progress}%`); // Assuming progress is a number
+        class: 'pl-4 text-white'
+      }, `${progress}%`);
     },
   },
+  {
+    accessorKey: 'chat',
+    header: () => h('div', {
+      class: 'text-blue-200 font-brand text-center'
+    }, 'Chat'),
+    cell: ({ row }) => h('div', {
+      class: 'flex justify-center'
+    }, [
+      h('button', {
+        onClick: () => openChat(row.original.id), // Assuming openChat is a method you'll define to handle chat opening
+        class: 'border px-4 py-2 rounded-xl hover:bg-blue-500 hover:text-white hover:ring-2 hover:ring-blue-300 hover:ring-offset-2 duration-300 hover:scale-95 active:scale-75 flex justify-center items-center space-x-2 bg-gray-50'
+      }, 'Chat')
+    ])
+
+  }
 ]
+
+
+// columns.push({
+//   id: 'chat',
+//   header: () => h("div",{class:'text-blue-200 font-brand text-center'},"Chat"),
+//   cell: ({ row }) => h('div', { class: 'flex justify-center' }, [
+//     h('button', {
+//       onClick: () => openChat(row.original.id), // Assuming openChat is a method you'll define to handle chat opening
+//       class: 'border px-4 py-2 rounded-xl hover:bg-blue-500 hover:text-white hover:ring-2 hover:ring-blue-300 hover:ring-offset-2 duration-300 hover:scale-95 active:scale-75 flex justify-center items-center space-x-2 bg-gray-50'
+//     }, 'Chat')
+//   ])
+// })
 
 const table = useVueTable({
   data: data?.value,
@@ -156,6 +227,12 @@ const table = useVueTable({
     get columnFilters() { return columnFilters.value },
   },
 })
+
+const openChat = (userId) => {
+  console.log(`Opening chat for user ID: ${userId}`);
+  // Implementation depends on how you handle chats in your app
+  // For example, this could dispatch a Vuex action or emit an event
+};
 
 
 
